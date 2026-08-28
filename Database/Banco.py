290d81d606e9679a2b7b2ee4c1d1a6ce80a5f8c3
Davy-Pereira <davytrabalho34@gmail.com>
2026-08-28 12:55:25 -0300

    Need fix this database with clean code

--- Database/Banco.py
+++ Database/Banco.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 Base.metadata.bind = create_engine('sqlite:///usuarios.db', echo=True)
+
+
 class Usuario(Base):
     __tablename__ = 'usuarios'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
-    document = Column(Integer)
+    Username = Column(String)
+    Password = Column(String)
+    Document = Column(Integer)
--- class/Login.py
+++ class/Login.py
@@ -1,9 +1,13 @@
+from Database.Banco import Usuario, Base
+
+Banco = Usuario(Base.metadata.bind)
+
 class Login:
-    def __init__(self):
-        self.users = []
+    def __init__(self, Banco):
+        self.Banco = Banco
 
     def login(self, username, password, doc):
-        for user in self.users:
+        for user in self.Banco.get_users():
             if user["Document"] == doc and user["Username"] == username and user["Password"] == password:
                 return True
-        return False
+        self.Banco.add_user(username, password, doc)
