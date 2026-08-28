737645b3ea24f7093d17ad113f7b295ab6f08da8
Davy-Pereira <davytrabalho34@gmail.com>
2026-08-28 11:18:59 -0300

    add Base

--- Data/Banco.py
+++ Data/Banco.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+Base.metadata.bind = create_engine('sqlite:///usuarios.db', echo=True)
 class Usuario(Base):
     __tablename__ = 'usuarios'
 
